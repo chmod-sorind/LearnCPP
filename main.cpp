aa09d9473504fe2fb1e2148975cfa26fd5a552e7
Sorin Diaconescu <diacconescu.sorin.g@gmail.com>
2017-08-07 16:44:55 -0400

    push code

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+
+    // Variable definition:
+    int a, b;
+    int c;
+    float f;
+
+    // actual initialization
+    a = 10;
+    b = 20;
+    c = a + b;
+
+    cout << c << endl ;
+
+    f = 70.0/c;
+    cout << f << endl ;
+
+    return 0;
+}
