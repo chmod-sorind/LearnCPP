e21ee7d64b0856e393c3a8d1e5e0cfd6e00e4972
chmod-sorind <diaconescu.sorin.g@gmail.com>
2017-08-09 07:03:14 -0400

    implement all 3 function to calculate sales taxes + 1 function to grab the sum from user.

--- main.cpp
+++ main.cpp
@@ -1,62 +1,60 @@
 #include <iostream>
+//#import <math>
 using namespace std;
 
 
 //Variable Declaration
-float pre_tax;
 
 //Function Declaration
 float getGST(float pt);
 float getPST(float pt);
+float getTotal(float pt, float gst, float pst);
 float get_user_amount();
 
 
 int main()
 {
     float preTax = get_user_amount();
-    cout << "Your sales amount is: " << preTax << "$" << endl;
+    cout << "Sales sum before taxes is: " << preTax << "$" << endl;
 
-    float gst = getGST(pre_tax);
-    cout << gst << endl;
+    float gst = getGST(preTax);
+    cout << "Global Sales Tax = " << gst << "$" << endl;
+
+    float pst = getPST(preTax);
+    cout << "Provincial Sales Tax = " << pst << "$" << endl;
+
+    float total = getTotal(preTax, gst, pst);
+    cout << "Total sales sum = " << total << "$" << endl;
 
     return 0;
 }
 
 float get_user_amount()
 {
+    float x;
     cout << "Enter sales amount:" << endl;
-    cin >> pre_tax;
-
-    return pre_tax;
+    cin >> x;
+    return x;
 }
 
 float getGST(float pt)
 {
-    float gstPercent;
-    float gst;
-
-    gstPercent = 5.0f;
-    gst = pt / 100 * 200;
-
-    return gst;
+    float gstPercent = 5.0f;
+    float g = gstPercent / 100 * pt;
+    return g;
 }
 
 
 float getPST(float pt)
 {
-    float pstP;
-    float pst;
-
-    pstP = 9.975f;
-    pst = pt / 100 * 100;
-
-    return pst;
+    float pstPercentage = 9.975f;
+    float p = pstPercentage / 100 * pt;
+    return p;
 }
 
 
 float getTotal(float pt, float gst, float pst)
 {
-    float after_tax = pt + gst + pst;
-
-    return after_tax;
+    float t = pt + gst + pst;
+    return t;
 }
